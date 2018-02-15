c7131fbf022785b41e934b71847ad088c3ee736a
adekoder <adwumiogunbiyi@gmail.com>
2018-02-15 15:00:04 +0100

    Added search feature and integration with the map

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,23 @@
-from flask import render_template
+from flask import render_template, request, jsonify, json
 from . import main
+from ..models import Location
+
+
+def location_schema(locations):
+    location_data = []
+    for location in locations:
+        location_data.append({
+            'location_id': location.location_id,
+            'location_name': location.location_name,
+            'coordinate': location.coordinate,
+        })
+    return jsonify(locations=location_data), 200
 
 @main.route('/')
 def index():
     return render_template('main/index.html')
 
-@main.route('/search')
+@main.route('/search', methods=['GET'])
 def search():
     return render_template('main/search.html')
 
@@ -13,8 +25,16 @@ def search():
 def explore():
     return render_template('main/explore.html')
 
+@main.route('/locations', methods=['POST'])
+def location():
+    data =  request.get_json(force=True)
+    location_name = data['location_name']
+    query_param = "%{}%".format(location_name)
+    print(query_param)
+    locations = Location.query.filter(Location.location_name.like(query_param)).all()
+    return location_schema(locations)
 
-@main.route('/map')
-def show_map():
-    return render_template('main/maps.html')
+@main.route('/map/<string:coords>')
+def show_map(coords):
+    return render_template('main/maps.html', coords=coords)
  
